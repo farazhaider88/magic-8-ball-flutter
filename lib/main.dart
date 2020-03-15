c0937d6b332eca0464a43698d74e19dda149cfad
farazhaider88 <farazhaider88@yahoo.com>
2020-03-15 14:47:24 +0500

    stateless / statefull widget
    added stateless and stateful widget to create flatbutton random generation

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,48 @@ import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+            child: Image.asset("images/ball$ballNumber.png"),
+            onPressed: () {
+              print('I got clicked');
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
